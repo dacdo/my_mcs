9bcc29812e8ccaf828d0d7c0201a9aaf4e7d9a9c
DacDo <mr.dacdo@gmail.com>
2019-07-12 05:04:03 +0800

    use git in policyfile

--- policyfiles/my_mcs.rb
+++ policyfiles/my_mcs.rb
@@ -1,5 +1,6 @@
 name 'my_mcs'
 run_list "recipe[my_mcs::default]"
 cookbook "my_mcs", path: '..'
-cookbook 'managed-chef-server', path: '../../managed-chef-server-cookbook'
+#cookbook 'managed-chef-server', path: '../../managed-chef-server-cookbook'
+cookbook 'managed-chef-server', git: 'https://github.com/mattray/managed-chef-server-cookbook.git'
 default_source :supermarket
