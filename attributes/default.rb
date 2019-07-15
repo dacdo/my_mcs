48d02fedc8b227a2040e35500912b3b667c61e3a
DacDo <mr.dacdo@gmail.com>
2019-07-15 11:03:18 +0800

    chef-server accept-license tru

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['mcs']['managed_user']['email'] = 'mr.dacdo@yahoo.com'
 default['mcs']['managed_user']['password'] = 'dacdo1234!'
 default['mcs']['skip_test'] = true
+default['chef-server']['accept_license'] = true
